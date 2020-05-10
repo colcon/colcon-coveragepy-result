60f77745920b331cdff5bb3284de67ee6c019ed7
Christophe Bedard <bedard.christophe@gmail.com>
2020-05-10 11:05:01 -0400

    Add tests

---
+++ colcon_coveragepy_result/task/coveragepy.py
@@ -0,0 +1,93 @@
+# Copyright 2020 Christophe Bedard
+# Licensed under the Apache License, Version 2.0
+
+from glob import glob
+import os
+from pathlib import Path
+from shutil import copy2
+import subprocess
+
+from colcon_core.logging import colcon_logger
+from colcon_core.plugin_system import satisfies_version
+from colcon_core.task import TaskExtensionPoint
+
+logger = colcon_logger.getChild(__name__)
+
+
+class CoveragePyTask(TaskExtensionPoint):
+    """Run coverage.py on a package."""
+
+    TASK_NAME = 'coveragepy'
+
+    def __init__(self):  # noqa: D107
+        super().__init__()
+        satisfies_version(TaskExtensionPoint.EXTENSION_POINT_VERSION, '^1.0')
+
+    async def coveragepy(self, *, additional_hooks=None):  # noqa: D102
+        pkg = self.context.pkg
+        args = self.context.args
+
+        logger.info('Running coverage.py on {pkg.name}'.format_map(locals()))
+
+        # Get list of .coverage files, depending on package type
+        pkg_build_path = Path(os.path.abspath(os.path.join(args.build_base, pkg.name)))
+        coverage_files = []
+        if 'ros.ament_cmake' == pkg.type:
+            coverage_files.extend(glob(str(pkg_build_path / 'pytest_cov/*/.coverage')))
+        elif 'ros.ament_python' == pkg.type:
+            coverage_files.append(str(pkg_build_path / '.coverage'))
+
+        if 0 == len(coverage_files):
+            logger.warning(
+                'No .coverage files for package {pkg.name} of type {pkg.type}'.format_map(locals())
+            )
+            return 0
+        logger.info(
+            'Coverage files for package {pkg.name}: {coverage_files}'.format_map(locals())
+        )
+
+        # Copy .coverage files to a new directory, because combining files deletes them
+        coveragepy_dir = self.get_package_combine_dir(args.build_base, pkg.name)
+        coveragepy_path = Path(coveragepy_dir)
+        coveragepy_path.mkdir(exist_ok=True)
+        logger.info('Copying coverage files to {coveragepy_dir}'.format_map(locals()))
+        coverage_files_copies = [
+            str(coveragepy_path / ('.coverage.' + str(i))) for i in range(len(coverage_files))
+        ]
+        for original, copy in zip(coverage_files, coverage_files_copies):
+            copy2(original, copy)
+
+        # Combine .coverage files
+        rc, stdout, stderr = coverage_combine(coverage_files_copies, coveragepy_dir)
+        if 0 == rc.returncode and args.verbose:
+            # Report
+            rc, stdout, stderr = coverage_report(coveragepy_dir, args.coverage_report_args)
+            if 0 == rc.returncode:
+                print('\n' + stdout.decode())
+        return rc.returncode
+
+    @staticmethod
+    def get_package_combine_dir(build_base, pkg_name):
+        """Get the directory in which to combine .coverage files for a given package."""
+        pkg_build_dir = os.path.abspath(os.path.join(build_base, pkg_name))
+        return str(os.path.abspath(os.path.join(pkg_build_dir, 'coveragepy')))
+
+
+def coverage_combine(files, cwd):
+    """Combine .coverage files."""
+    cmd = ['coverage', 'combine'] + files
+    process = subprocess.Popen(cmd, cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    if 0 != process.returncode:
+        logger.error('coverage_combine() failed: ' + stderr.decode())
+    return process, stdout, stderr
+
+
+def coverage_report(cwd, additional_args):
+    """Produce a report for a .coverage file."""
+    cmd = ['coverage', 'report'] + (additional_args or [])
+    process = subprocess.Popen(cmd, cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    if 0 != process.returncode:
+        logger.error('coverage_report() failed: ' + stderr.decode())
+    return process, stdout, stderr
---
+++ colcon_coveragepy_result/verb/coveragepy_result.py
@@ -0,0 +1,129 @@
+# Copyright 2020 Christophe Bedard
+# Licensed under the Apache License, Version 2.0
+
+from collections import OrderedDict
+import os
+from pathlib import Path
+
+from colcon_core.command import add_log_level_argument
+from colcon_core.event_handler import add_event_handler_arguments
+from colcon_core.executor import add_executor_arguments
+from colcon_core.executor import execute_jobs
+from colcon_core.executor import Job
+from colcon_core.logging import colcon_logger
+from colcon_core.package_selection import add_arguments as add_packages_arguments
+from colcon_core.package_selection import get_package_descriptors
+from colcon_core.package_selection import select_package_decorators
+from colcon_core.plugin_system import satisfies_version
+from colcon_core.task import TaskContext
+from colcon_core.topological_order import topological_order_packages
+from colcon_core.verb import check_and_mark_build_tool
+from colcon_core.verb import VerbExtensionPoint
+
+from ..task.coveragepy import coverage_combine
+from ..task.coveragepy import coverage_report
+from ..task.coveragepy import CoveragePyTask
+
+logger = colcon_logger.getChild(__name__)
+
+
+class CoveragePyResultVerb(VerbExtensionPoint):
+    """Collect and display coverage.py results."""
+
+    def __init__(self):  # noqa: D107
+        super().__init__()
+        satisfies_version(VerbExtensionPoint.EXTENSION_POINT_VERSION, '^1.0')
+
+    def add_arguments(self, *, parser):  # noqa: D102
+        parser.add_argument(
+            '--build-base',
+            default='build',
+            help='The base path for all build directories (default: %(default)s)',
+        )
+        parser.add_argument(
+            '--coveragepy-base',
+            default='coveragepy',
+            help='The path for coveragepy artifacts (default: %(default)s)',
+        )
+        parser.add_argument(
+            '--coverage-report-args',
+            nargs='*', metavar='*', type=str.lstrip,
+            help="Pass arguments to 'coverage report'. Arguments matching "
+                 'other options must be prefixed by a space, '
+                 'e.g. --coverage-report-args " --help"',
+        )
+        parser.add_argument(
+            '--verbose',
+            action='store_true',
+            help='Show coverage results for individual packages and overall',
+        )
+        add_packages_arguments(parser)
+        add_log_level_argument(parser)
+        add_executor_arguments(parser)
+        add_event_handler_arguments(parser)
+
+    def main(self, *, context):  # noqa: D102
+        build_base = context.args.build_base
+        check_and_mark_build_tool(build_base)
+
+        # Combine each package's .coverage files
+        coveragepy_pkgs = self._get_coveragepy_packages(context)
+        jobs = OrderedDict()
+        for pkg in coveragepy_pkgs:
+            task_context = TaskContext(
+                pkg=pkg,
+                args=context.args,
+                dependencies=OrderedDict(),
+            )
+            task = CoveragePyTask()
+            job = Job(
+                identifier=pkg.name,
+                dependencies=set(),
+                task=task,
+                task_context=task_context,
+            )
+            jobs[pkg.name] = job
+        rc = execute_jobs(context, jobs)
+
+        # Combine all packages' .coverage files
+        coverage_files = [
+            str(Path(CoveragePyTask.get_package_combine_dir(build_base, pkg.name)) / '.coverage')
+            for pkg in coveragepy_pkgs
+        ]
+        # Filter out non-existing files in case processing failed for some packages
+        coverage_files = list(filter(os.path.exists, coverage_files))
+        logger.info('Coverage files: {coverage_files}'.format_map(locals()))
+        coveragepy_base_dir = str(os.path.abspath(context.args.coveragepy_base))
+        Path(coveragepy_base_dir).mkdir(exist_ok=True)
+        rc, stdout, stderr = coverage_combine(coverage_files, coveragepy_base_dir)
+        if 0 == rc.returncode and context.args.verbose:
+            rc, stdout, stderr = coverage_report(
+                coveragepy_base_dir,
+                context.args.coverage_report_args,
+            )
+            if 0 == rc.returncode:
+                print('\n' + stdout.decode())
+        return rc.returncode
+
+    @staticmethod
+    def _get_coveragepy_packages(context, additional_argument_names=None):
+        """Get packages that have coverage.py results."""
+        descriptors = get_package_descriptors(
+            context.args,
+            additional_argument_names=additional_argument_names,
+        )
+        decorators = topological_order_packages(descriptors, recursive_categories=('run', ))
+        select_package_decorators(context.args, decorators)
+        coveragepy_pkgs = []
+        for decorator in decorators:
+            if not decorator.selected:
+                continue
+            pkg = decorator.descriptor
+            if pkg.type in ['ros.ament_cmake', 'ros.ament_python']:
+                coveragepy_pkgs.append(pkg)
+            else:
+                logger.info(
+                    'Specified package {pkg.name} is not a coverage.py-compatible '
+                    'package. Not collecting coverage information.'.format_map(locals())
+                )
+        return coveragepy_pkgs
