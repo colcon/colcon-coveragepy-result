a3e1af6d62044a1ba9d01a0ceabd422bb1dc9b3d
Christophe Bedard <bedard.christophe@gmail.com>
2020-05-10 11:03:42 -0400

    Add package files

---
+++ colcon_coveragepy_result/__init__.py
@@ -0,0 +1,5 @@
+# Copyright 2020 Christophe Bedard
+# Licensed under the Apache License, Version 2.0
+
+
+__version__ = '0.0.1'
---
+++ setup.py
@@ -0,0 +1,7 @@
+# Copyright 2020 Christophe Bedard
+# Licensed under the Apache License, Version 2.0
+
+import setuptools
+
+
+setuptools.setup()
